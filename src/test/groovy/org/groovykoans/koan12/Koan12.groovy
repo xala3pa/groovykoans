9fd9453dc34a0213e7f949e494d75df4cfb1ed93
xala3pa <salazar3pa@gmail.com>
2014-05-21 22:14:49 +0200

    resolving koan12

--- src/test/groovy/org/groovykoans/koan12/Koan12.groovy
+++ src/test/groovy/org/groovykoans/koan12/Koan12.groovy
@@ -38,12 +38,10 @@ class Koan12 extends GroovyTestCase {
         // Use shouldFail() to make this unit test pass. Try to be specific about the exception type.
 
         // ------------ START EDITING HERE ----------------------
-        shouldFail(MissingMethodException) {
-            // ------------ STOP EDITING HERE  ----------------------
+	shouldFail(MissingMethodException) {
             Integer.nonExistentMethod()
-            // ------------ START EDITING HERE ----------------------
         }
-        // ------------ STOP EDITING HERE  ----------------------
+	// ------------ STOP EDITING HERE  ----------------------
 
     }
 
@@ -57,10 +55,9 @@ class Koan12 extends GroovyTestCase {
         Factory factory = new Factory([1, 2])
         long startTime = System.currentTimeMillis()
         // ------------ START EDITING HERE ----------------------
-        def stub = new Expando();
-        stub.work = { number -> number + 10 }
-        factory.setWorker(stub)
-        // ------------ STOP EDITING HERE  ----------------------
+        
+	
+	// ------------ STOP EDITING HERE  ----------------------
         factory.work()
         long endTime = System.currentTimeMillis()
         assert endTime - startTime < 2000
@@ -72,9 +69,9 @@ class Koan12 extends GroovyTestCase {
         def staticFactory = new StaticFactory(numbers: [2, 3])
         startTime = System.currentTimeMillis()
         // ------------ START EDITING HERE ----------------------
-        Worker worker = { number -> number + 10 } as Worker
-        StaticFactory.metaClass.'static'.getWorker = { worker }
-        // ------------ STOP EDITING HERE  ----------------------
+        
+	
+	// ------------ STOP EDITING HERE  ----------------------
         staticFactory.work()
         endTime = System.currentTimeMillis()
         assert endTime - startTime < 2000
